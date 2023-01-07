dc4811c5b0bd2e51f5f2ec5a42ed9ff778dea7eb
Thalyson Pin <thalyson_emanuels.p@hotmail.com>
2023-01-07 08:34:25 -0400

    att

--- src/axios/config.js
+++ src/axios/config.js
@@ -4,7 +4,7 @@ export const popularMovies = axios.create({
     baseURL: "https://api.themoviedb.org/3/movie/popular?api_key=8336f63b9b0cba77e8173ea337f57133&language=pt-BR&page=1",
 })
 
-export const searchMovies = "https://api.themoviedb.org/3/search/movie/"
+export const searchMovies = "https://api.themoviedb.org/3/search/movie/?api_key=8336f63b9b0cba77e8173ea337f57133"
 
 export const imageMovies = "https://image.tmdb.org/t/p/w500/"
 
--- src/pages/Search/index.jsx
+++ src/pages/Search/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSearchParams } from 'react-router-dom'
 import { imageMovies, searchMovies, popularMovies } from '../../axios/config'
 import MovieCard from '../../components/MovieCard.jsx'
@@ -12,9 +12,31 @@ function Search() {
     const [movies, setMovies] = useState([])
     const query = searchParams.get("q")
 
+    async function getSearcherMovies(){
+        await searchMovies.get()
+            .then(function (response) {
+                console.log(response.data.results)
+                const results = response.data.results
+                setMovies(results)
+    
+            })
+            .catch(function(error){
+                console.log(error)
+            })
+    
+        }
+    
+
+    useEffect(()=>{
+        const searchWithQueryURL = `${searchMovies}&query=${query}`
+        getSearcherMovies(searchWithQueryURL);
+
+    }, [query])
+
     return (
         <div className='container'>
             <h2>Resultados para: <span className='movie-query'>{query}</span></h2>
+            {movies.length > 0 ? movies.map((movie) => <MovieCard key={movie.id} movie={movie}/>) : <p>Loading...</p>}
 
         </div>
     )
